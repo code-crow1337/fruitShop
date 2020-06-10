e3241ee4306167a5b6cd243765b1cf5250a6d9db
Josephine Een <josephine.een@gmail.com>
2020-06-10 21:42:45 +0200

    Added small changes to the css so I can reuse classes for the other pages

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.sendFile('index.html');
 });
+app.get('/about', (req, res) => {
+  res.sendFile('about.html');
+});
 app.listen(1337, () => {
   console.log('Listening on port 1337');
 })
